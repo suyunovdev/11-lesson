c7d222dba1613ef431434fa09e7bc41be40dc679
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2024-04-18 12:50:00 +0500

    finished

---
+++ main.js
@@ -0,0 +1,85 @@
+// 1. 1-raqam 2-songa teng boʻlganda true qaytaruvchi funksiya yarating; aks holda false qaytaring:
+
+//   isSameNum(4, 8) ➞ false
+//   isSameNum(2, 2) ➞  true
+//   isSameNum(2, "2") ➞ false
+
+            // function counter(sum1,sum2){
+            // return sum1===sum2;
+            // }
+            // console.log(counter(4,2));
+            // console.log(counter(2,2));
+            // console.log(counter(2,"2"));
+
+
+// 2. Ikkita raqam berilgan bo'lsa, ikkala raqamning yig'indisi 100 dan kichik bo'lsa, true qiymatini qaytaring. Aks holda, false qaytaring:
+
+//   lessThan100(22, 15) ➞ true
+//   // 22 + 15 = 37
+
+//   lessThan100(83, 34) ➞ false
+//   // 83 + 34 = 117
+
+//   lessThan100(3, 77) ➞ true
+
+
+                // function sum (num1,num2){
+                //     return (num1 + num2)<100;
+                // }
+                // console.log(sum(22,15));
+                // console.log(sum(83,34));
+                // console.log(sum(3,77));
+                
+
+
+// 3. Ikkita butun sonni (soat, daqiqa) oladigan, ularni soniyalarga aylantiruvchi va qo‘shuvchi funksiya yozing:
+
+//   convert(1, 3) ➞ 3780
+//   convert(2, 0) ➞ 7200
+//   convert(0, 0) ➞ 0
+
+                    // function counter(soat,daqiqa){
+                    //     return soat*3600+daqiqa*60
+                    // }
+                    // console.log(counter(1,3));
+                    // console.log(counter(2,0));
+                    // console.log(counter(0,0));
+
+// 4. Quyidagi funktsiya faqat 7 qabul qilsagina true, yoqsa false qaytaradi. Lekin code'da xatolik mavjud. Uni toping.
+//   Code: 
+//   function isSeven(x) {
+// 	  return x="7"?false:true:false;
+//   }
+                    // function isSeven(x) {
+                    //         return x=== 7 ?true:false;
+                    //     }
+                    //     console.log(isSeven(7))
+                        
+
+// 5. Raqamni yagona argument sifatida qabul qiladigan va u noldan kichik yoki teng bo‘lsa, true, aks holda false qiymatini qaytaradigan funksiya yarating:
+
+//   lessThanOrEqualToZero(5) ➞ false
+//   lessThanOrEqualToZero(0) ➞ true
+//   lessThanOrEqualToZero(-2) ➞ true
+
+                    // function sum (num){
+                    //     return num <=0;
+                    // }
+                    // console.log(sum(0))
+
+
+// 6. Ikki argument oladigan funksiya yarating. Ikkala argument ham a va b butun sonlardir. Agar ulardan biri 10 bo'lsa yoki yig'indisi 10 bo'lsa, true qiymatini qaytaring, aks holda false:
+
+//   makesTen(9, 10) ➞ true
+//   makesTen(9, 9) ➞ false
+//   makesTen(1, 9) ➞ true  
+
+                    // function counter (a,b){
+                    //     return a===10 || b===10 || a+b===10; 
+                    // }
+                    // console.log(counter(9,10));
+                    // console.log(counter(9,9));
+                    // console.log(counter(1,9));
+
+
+
